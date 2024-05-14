2b017bc87eb534b0f34e350bfba2f4ab47a0b17d
FullTimeScam <418.gingin@gmail.com>
2024-05-14 17:45:23 +0900

    onChange 이벤트를 활용해서 입력값을 확인하기

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [content, setContent] = useState("Rnpr초기값");
+
   const onSubmitChat = (e) => {
     e.preventDefault();
-    console.log(e);
+    //text에 질문을 입력 받아서 서버로 보내주는 작업이 필요함
+  };
+  const onChangeContent = (e) => {
+    console.log(e.target.value);
   };
 
   return (
     <div>
       <form onSubmit={onSubmitChat}>
-        <input type="text" />
+        <input type="text" value={content} onChange={onChangeContent} />
         <input type="submit" />
       </form>
     </div>
