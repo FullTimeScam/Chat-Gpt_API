04f51b957c35da621d0f509a34bfa3a6019761a3
FullTimeScam <418.gingin@gmail.com>
2024-05-14 16:42:18 +0900

    버튼을 링크로 바꾸기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <header className="bg-blue-100 px-2 flex justify-center gap-8">
-      <button>질문하기</button>
-      <button>내 질문 리스트</button>
+      <Lnik>질문하기</Lnik>
+      <Lnik>내 질문 리스트</Lnik>
     </header>
   );
 };
