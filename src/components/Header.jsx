0e5d4c5e3957bb2f91c393ee21d55bdd63581022
FullTimeScam <418.gingin@gmail.com>
2024-05-14 16:52:14 +0900

    Link랑 스타일 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="bg-blue-100 px-2 flex justify-center gap-8">
-      <Lnik>질문하기</Lnik>
-      <Lnik>내 질문 리스트</Lnik>
+      <Link className="link-style" to="/">
+        질문하기
+      </Link>
+      <Link className="link-style" to="chat_list">
+        내 질문 리스트
+      </Link>
     </header>
   );
 };
