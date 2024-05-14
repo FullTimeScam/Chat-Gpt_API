e58f03d61c9b597bc5dd0bea7810311de3ff696e
FullTimeScam <418.gingin@gmail.com>
2024-05-14 16:40:37 +0900

    버튼 세팅

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 const Header = () => {
-  return <header className="px-2 flex justify-center gap-8">Header</header>;
+  return (
+    <header className="bg-blue-100 px-2 flex justify-center gap-8">
+      <button>질문하기</button>
+      <button>내 질문 리스트</button>
+    </header>
+  );
 };
 
 export default Header;
