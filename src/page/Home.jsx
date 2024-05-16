c958379faad0af2cf9c4a769440bcc1070bfedf1
FullTimeScam <418.gingin@gmail.com>
2024-05-16 10:24:27 +0900

    디자인 쪼금 바꿈

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
   return (
     <div className="mt-8 flex justify-center">
-      <form onSubmit={onSubmitChat}>
+      <form className="flex" onSubmit={onSubmitChat}>
         <input
           className="text-2xl p-2 focus:outline-none rounded-lg border-2 border-orange-200 focus:border-orange-400"
           type="text"
@@ -21,7 +21,7 @@ const Home = () => {
         />
 
         <button
-          className="ml-4 bg-orange-200 text-2xl px-4 py-[10px] rounded-full shadow-md shadow-orange-300 hover:bg-orange-300"
+          className="ml-4 flex justify-center items-center bg-orange-200 text-2xl px-4 py-[10px] rounded-full shadow-md shadow-orange-300 hover:bg-orange-300"
           type="submit"
         >
           <TbRobot /> 검색
